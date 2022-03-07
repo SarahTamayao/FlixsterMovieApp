0cb050245a03e0d61bcd0eb5be3c06eaff7cd157
hassan osman <HassanIbra368@gmail.com>
2022-03-07 02:30:22 -0500

    Added youtubeView, added ratingbar, and many other changed

--- app/src/main/java/com/codepath/flixster/MovieAdapter.kt
+++ app/src/main/java/com/codepath/flixster/MovieAdapter.kt
@@ -1,6 +1,7 @@
 package com.codepath.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
+const val MOVIE_EXTRA= "MOVIE_EXTRA"
+private const val TAG ="MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>):
     RecyclerView.Adapter<MovieAdapter.viewHolder>() {
 
@@ -25,16 +28,28 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount()= movies.size
 
-    inner class viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
         private val ivposter = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview= itemView.findViewById<TextView>(R.id.tvOverview)
 
+        init {
+            itemView.setOnClickListener(this)
+        }
         fun bind(movie: Movie) {
             Glide.with(context).load(movie.posterImageUrl).into(ivposter)
             tvTitle.text =movie.title
             tvOverview.text = movie.overview
 
+        }
+
+        override fun onClick(v: View?) {
+            val movie = movies[adapterPosition]
+
+
+            val intent = Intent(context,DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA,movie)
+            context.startActivity(intent)
         }
     }
 
